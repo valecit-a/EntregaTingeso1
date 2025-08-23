ec9e5efdf7cbd99babca86c8f5977f3eade36c5a
valecita <128949761+valecit-a@users.noreply.github.com>
2025-08-22 20:43:56 -0400

    Agregando springBoot

---
+++ toolrent/src/main/java/cl/toolrent/toolrent/ToolrentApplication.java
@@ -0,0 +1,13 @@
+package cl.toolrent.toolrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolrentApplication.class, args);
+	}
+
+}
---
+++ toolrent/src/test/java/cl/toolrent/toolrent/ToolrentApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.toolrent.toolrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
